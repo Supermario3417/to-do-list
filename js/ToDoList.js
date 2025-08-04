4b5e887277f3965107fa6728013aca401489f592
3417 <Arjunshetty1300@outlook.com>
2025-08-04 12:20:19 -0500

    Files added

---
+++ js/ToDoList.js
@@ -0,0 +1,16 @@
+function addItem() {
+    var item = document.getElementById("box");
+    var list_item = document.getElementById("list_item");
+    if (item.value != "") {
+        var make_li = document.createElement("li");
+        make_li.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(make_li);
+        item.value="";
+
+        make_li.onclick = function(){
+            this.parentNode.removeChild(this);
+        }
+    } else {
+        alert("Invalid input");
+    }
+}
